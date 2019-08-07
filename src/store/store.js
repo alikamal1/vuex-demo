0854ccb99e073c78e9b1c3269dace925d5d8a200
Ali Kamal <alikamal1@yahoo.com>
2019-08-07 10:16:43 +0300

    comment

--- src/store/store.js
+++ src/store/store.js
@@ -34,12 +34,28 @@ export const store = new Vuex.Store({
 
 // this.$store.dispatch("moveToSecondArray", element);
 
-// moveToFirstArray(state, element) {
-//     state.firstArray.push(element);
-//     state.secondArray.splice(state.secondArray.indexOf(element), 1);
-// },
+// state
+// state: {
+//   firstArray: [{id: 1, name: 'User 1'},
+//                {id: 2, name: 'User 2'},
+//                {id: 3, name: 'User 3'},
+//               ]
+//     }
 
+// getter
+// firstArray(state) {
+//   return state.firstArray
+// }
+
+
+// action
+// moveToFirstArray(context, element) {
+//   context.commit('moveToFirstArray', element);
+// }
+
+
+// mutation
 // moveToFirstArray(state, element) {
-//     state.firstArray.push(element);
-//     state.secondArray.splice(state.secondArray.indexOf(element), 1);
-// },
+//   state.firstArray.push(element);
+//   state.secondArray.splice(state.secondArray.indexOf(element), 1);
+// }
